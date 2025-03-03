a094e483a050922b246a870f8abb88d0aa097373
Alexander <asanjar988@g.educaand.es>
2025-03-03 18:13:50 +0100

    Creación de la clase "Audiolibro" y el constructor de la clase "RecursoDigital".

--- src/main/java/RecursoDigital.java
+++ src/main/java/RecursoDigital.java
@@ -5,6 +5,12 @@ public abstract class RecursoDigital {
     protected int anioPublicacion;
     protected boolean disponible;
 
+    public RecursoDigital(String autor, String titulo, int anioPublicacion, boolean disponible){
+        this.autor = autor;
+        this.titulo = titulo;
+        this.anioPublicacion = anioPublicacion;
+        this.disponible = disponible;
+    }
 
     public abstract boolean prestar();
 
