00b4aba66351114f627fa5b39e6ce68b66777544
Nicolas Jimenez Gutierrez <njimgut1905@g.educaand.es>
2025-03-03 18:21:29 +0100

    He terminado de hacer Administrador

--- src/main/java/Administrador.java
+++ src/main/java/Administrador.java
@@ -1,3 +1,12 @@
-public class Administrador {
+public class Administrador extends Usuario {
 
+    private int nivelAcceso;
+
+    public boolean agregarRecurso(RecursoDigital recurso){
+        return true;
+    }
+
+    public boolean eliminarRecurso(String titulo){
+        return true;
+    }
 }
