551dc8ccd790be5e7e24027a1da465d53df0b7cd
JoaquinRuizJimenez <jruijim2512@g.educaand.es>
2025-03-03 17:26:51 +0100

    Plantilla RecursoDigital

---
+++ src/main/java/RecursoDigital.java
@@ -0,0 +1,15 @@
+public abstract class RecursoDigital {
+
+    protected String autor;
+    protected String titulo;
+    protected int anioPublicacion;
+    protected boolean disponible;
+
+
+    public abstract boolean prestar();
+
+    public abstract void devolver();
+
+}
+
+
