89483e7456d9569cf02babd03d6b29bcb7163b90
NicoJG27 <njimgut1905@g.educaand.es>
2025-03-10 16:05:40 +0100

    Cambio de código en clase Administrador.java

--- src/main/java/Administrador.java
+++ src/main/java/Administrador.java
@@ -1,12 +1,19 @@
 public class Administrador extends Usuario {
 
     private int nivelAcceso;
+    private ArrayList<RecursoDigital> recursos = new ArrayList<>();
 
-    public boolean agregarRecurso(RecursoDigital recurso){
-        return true;
+    public boolean agregarRecurso(RecursoDigital recurso) {
+        return recurso != null && recursos.add(recurso);
     }
 
-    public boolean eliminarRecurso(String titulo){
-        return true;
+    public boolean eliminarRecurso(String titulo) {
+        for (int i = 0; i < recursos.size(); i++) {
+            if (recursos.get(i).getTitulo().equalsIgnoreCase(titulo)) {
+                recursos.remove(i);
+                return true;
+            }
+        }
+        return false;
     }
 }
